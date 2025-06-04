6c3d7b413f96d241ff4a647fa8c9fe2d27841e66
nnippon99 <adamayyad1999@hotmail.com>
2025-06-04 15:49:07 -0300

    waydroid: set PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK to true
    Signed-off-by: nnippon99 <adamayyad1999@hotmail.com>

--- device.mk
+++ device.mk
@@ -232,6 +232,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Speed profile services and wifi-service to reduce RAM and storage.Add commentMore actions
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 
 # Binder IPC
 PRODUCT_PACKAGES += \
